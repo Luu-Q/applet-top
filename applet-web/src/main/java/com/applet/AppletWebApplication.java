da2f5514d002c81e14f31ed2e86aaecfd7f8111f
luning <luning@yktour.com.cn>
2019-03-25 17:34:38 +0800

    EnableFeignClients开启

--- applet-web/src/main/java/com/applet/AppletWebApplication.java
+++ applet-web/src/main/java/com/applet/AppletWebApplication.java
@@ -6,11 +6,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Import;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 //@RibbonClient(name="clean-sms",configuration = RibbonIRuleConfig.class)
 @EnableHystrix
+@EnableFeignClients
 @EnableEurekaClient
 @EnableSwagger2
 @SpringBootApplication(scanBasePackages = "com.applet.*")
